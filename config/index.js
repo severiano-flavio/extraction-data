fef22b8dea09f6c3d6441945e0768f1eb6af8c05
Flávio <severiano.flavio18@gmail.com>
2022-05-31 09:16:29 -0300

    Ajusta variáveis de ambiente

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const crawler = require('../src/controller/crawl');
 const scraping = require('../src/controller/scrap');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const listener = app.listen(port, () => {
   console.log('app is running on port: ' + listener.address().port);
